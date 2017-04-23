4a27370830ebb1c3fbe4070a8827e1f940168b08
Krzysztof Hamerszmit <kkhamer@gmail.com>
2017-04-23 14:34:25 +0200

    Client login

--- client.js
+++ client.js
@@ -2,3 +2,36 @@
 
 const io = require('socket.io-client');
 const socket = io('http://localhost:4000');
+
+// setInterval(()=> {
+//     console.log('wysyłam wiadomość');
+//     socket.emit('msg', 'Hello word!'); //,sg -dowolne słowo
+// }, 2000);
+
+// socket.on('msg', (data)=> {
+//     console.log("serwer mówi" + data);
+// });
+
+//Umożliwia wpisywanie w konsoli klienta wiadomości
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.on('line', (line) => {
+    socket.emit('sendMsg', line);
+});
+
+readline.prompt();
+
+socket.on('sendResp', data => {
+    console.log(`${data.user}: ${data.msg}\n`);
+})
+
+
+
+//ZADANIE sprawdzeie czy to komenda czy tekst
+//wysłanie na serwer - 2 sposoby
+//serwer - jak tekst to wysyłamy, jak komenda to client.name = name;
+
+
--- server.js
+++ server.js
@@ -3,15 +3,48 @@
 const io = require('socket.io')(); //w () można wpisywać dodatkowe rzeczy - przez to się uruchomi, inaczej tylko deklarujesz.
 
 io.on('connection', (client)=> {
-    console.log('nowy klient połączył się');
+    console.log('nowy klient polaczyl sie');
 
-    client.on('disconnect', ()=> {
+    // client.on('msg', (data) => {
+    //     console.log('Klient wysłał:' + data);
+    //     client.emit('msg', data);
+    // });
+
+    // client.on('msg2', data => {
+    //     console.log("Klient mowi " + data);
+    //     io.emit('sendIt', data);
+    // });
+
+    client.on('sendMsg', data => {
+        if (data.startsWith('/login')) {
+            var username = data.substring(7);
+            client.name = username;
+            console.log(client.name);
+        } else {
+           io.emit('sendResp', {
+               msg: data,
+               user: client.name || 'Anonim'
+           });
+        }
+    });
+
+    client.on('disconnect', ()=> {              //server jestteż event emmiterem
         console.log('klient rozłączył się');
     });
+
+    // setTimeout(() => {
+    //     client.disconnect(() => {
+    //         console.log('cośtam');
+    //     })
+    // })
 });
 
+// let count = 0;
+// setInterval(() => {
+//     count++;
+//     io.emit('msg', 'Jestem groźny serwer'+ count);
+// }, 2000);
 
-
-console.log('to jest serwer');
+console.log('Serwer stoi!');
 
 io.listen(4000);
